04ed623484c818d44ba709f57f077f640f99b07b
dacruz2021 <72949859+dacruz2021@users.noreply.github.com>
2022-01-10 17:28:05 -0600

    Rendered "this is my change"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  return <div className="app">Trivia!- This is my change</div>;
 }
 
 export default App;
