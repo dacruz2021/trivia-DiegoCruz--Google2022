9283f5954c6ea6d320a92ca8e3aa60989a823a6d
dacruz2021 <72949859+dacruz2021@users.noreply.github.com>
2022-02-14 17:22:59 -0600

    Day 1 complete ( working on day 2 goal) We got through a lot of code with plenty of good comments. We just need to fix our answers to render the choices given

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,38 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+//(day 1) this is our component "Questions", we used props and rendered the message in our App
+function Questions(props) {
+  return <div>
+    {props.ques}
+    <Answer/>
+  </div>;
+}
+
+//(day 1) this is our NextQuestion component that stores a button 
+function NextQuestion() {
+  return <div> <button>Next Question</button> </div>
+}
+//(day 2) our answer component
+function Answer(props){
+  return <div>Answer Goes Here</div>;
+}
+//
+// (day 1) we took our Questions components and used props to render our questions
+//(day 1) the variable holds which question we are on, so we call it when we use data
 function App() {
-  return <div className="app">Trivia!- This is my change</div>;
+  var currentQuestion = 0;
+
+  return (
+    <div className="app">
+      <h1>Trivia</h1>
+      <Questions ques={data[currentQuestion].question.text} Answer= />
+      <NextQuestion/>
+    </div>
+  );
 }
 
 export default App;
+// im not sure tbh but i think you have to render them first
+// goal 1 the thir bock
+// isnt that it u got it dont u?
